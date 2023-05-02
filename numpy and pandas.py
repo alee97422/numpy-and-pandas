6a624223ce1b3ea1ba41da420fb5f67daec649b8
alee97422 <alee97422@gmail.com>
2023-05-02 10:18:07 -0500

    this is a commit to add the pydoc and the certificate to this new repo

---
+++ numpy and pandas.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+# Creating a dataframe for the first set of bank clients
+bank_df_1 = pd.DataFrame({
+    'Bank Client ID': ['1', '2', '3', '4', '5'],
+    'First Name': ['Nancy', 'Alex', 'Shep', 'Max', 'Allen'], 
+    'Last Name': ['Rob', 'Ali', 'George', 'Mitch', 'Steve']
+})
+
+# Creating a dataframe for the second set of bank clients
+bank_df_2 = pd.DataFrame({
+    'Bank Client ID': ['6', '7', '8', '9', '10'],
+    'First Name': ['Bill', 'Dina', 'Sarah', 'Heather', 'Holly'], 
+    'Last Name': ['Christian', 'Mo', 'Steve', 'Bob', 'Michelle']
+})
+
+# Creating a dataframe for all bank clients' annual salary information
+bank_df_salary = pd.DataFrame({
+    'Bank Client ID': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    'Annual Salary [$/year]': [25000, 35000, 45000, 48000, 49000, 32000, 33000, 34000, 23000, 22000]
+})
+
+# Merging all dataframes based on 'Bank Client ID'
+bank_df_all = pd.merge(pd.concat([bank_df_1, bank_df_2]), bank_df_salary, on='Bank Client ID')
+
+# Adding a new bank client to the dataframe
+new_client_df = pd.DataFrame({
+    'Bank Client ID': ['11'],
+    'First Name': ['Ry'], 
+    'Last Name': ['Aly'],
+    'Annual Salary [$/year]' : [1000]
+})
+
+# Appending the new client to the existing dataframe
+new_df = bank_df_all.append(new_client_df, ignore_index=True)
